d9fc26008fb84474b50d2b208d2746525d9aaa06
Your Name <you@example.com>
2024-01-22 17:08:58 +0900

    section03

--- src/main/java/moviebuddy/domain/CsvMovieReader.java
+++ src/main/java/moviebuddy/domain/CsvMovieReader.java
@@ -17,12 +17,12 @@ import moviebuddy.util.FileSystemUtils;
 
 public class CsvMovieReader implements MovieReader {
 
-	@Override
     /**
      * 영화 메타데이터를 읽어 저장된 영화 목록을 불러온다.
      * 
      * @return 불러온 영화 목록
      */
+	@Override
     public List<Movie> loadMovies() {
   
         try {
